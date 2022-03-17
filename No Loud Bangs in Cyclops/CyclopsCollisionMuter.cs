4ec80bf298b918741b92510aa57acd256fd59390
GreaterDane42 <starlitvagabond@gmail.com>
2022-03-17 18:18:53 -0400

    Play cyclops collision sound when impacting static objects that have illogically low mass.

--- No Loud Bangs in Cyclops/CyclopsCollisionMuter.cs
+++ No Loud Bangs in Cyclops/CyclopsCollisionMuter.cs
@@ -12,33 +12,55 @@ namespace No_Loud_Bangs_in_Cyclops
         [HarmonyPrefix]
         public static bool Prefix(Collision col)
         {
-            bool makeSound;
+            bool playSound;
             string logMessage = $"{col.gameObject.name} ({col.gameObject.tag}) collided with cyclops.";
 
             if (col.gameObject.name == terrainObjectName)
             {
-                makeSound = true;
+                playSound = true;
             }
             else
             {
-                Rigidbody rigidbody = Utils.FindAncestorWithComponent<Rigidbody>(col.gameObject);
-                if (rigidbody == null)
+                // If an object doesn't have a LiveMixin, then it's likely a large solid object
+                // such as a grand reef anchor pod, in which case we want to play an impact sound.
+                // (Ideally we could filter these static decor items by mass, as we do for objects
+                // with a LiveMixin, but some of their masses are illogically lower than small fish.)
+                //
+                LiveMixin liveMixin = col.gameObject.GetComponent<LiveMixin>();
+                if (liveMixin == null)
                 {
-                    makeSound = true;
-                    logMessage += " Rigid body is NULL!";
+                    liveMixin = Utils.FindAncestorWithComponent<LiveMixin>(col.gameObject);
+                }
+
+                if (liveMixin == null)
+                {
+                    playSound = true;
+                    logMessage += " No LiveMixin.";
                 }
                 else
-                {
-                    makeSound = (rigidbody.mass > spadefishMassVanilla);
-                    logMessage += $" Mass {rigidbody.mass} (min {spadefishMassVanilla}).";
+                { 
+                    // Only play the cyclops' metal hull impact sound if the object has a mass larger
+                    // than the heaviest small fish.
+                    //
+                    Rigidbody rigidbody = Utils.FindAncestorWithComponent<Rigidbody>(col.gameObject);
+                    if (rigidbody == null)
+                    {
+                        playSound = true;
+                        logMessage += " No rigid body!";
+                    }
+                    else
+                    {
+                        playSound = (rigidbody.mass > spadefishMassVanilla);
+                        logMessage += $" Mass {rigidbody.mass} (min {spadefishMassVanilla}).";
+                    }
                 }
             }
 
-            logMessage += makeSound ? " Continue." : " Cancelling sound!";
-            //QModServices.Main.AddCriticalMessage(logMessage);
+            logMessage += playSound ? " Continue." : " Cancelling sound!";
+            QModServices.Main.AddCriticalMessage(logMessage);
             Logger.Log(Logger.Level.Info, logMessage);
 
-            return makeSound;
+            return playSound;
         }
 
         private readonly static string terrainObjectName = "override collider";
