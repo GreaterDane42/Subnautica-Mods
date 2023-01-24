ab3c620d137643e7a6b9e6f6f63878bf29f891c3
GreaterDane42 <101679252+GreaterDane42@users.noreply.github.com>
2023-01-24 18:41:10 -0500

    correct version number

--- Scan for More/Main.cs
+++ Scan for More/Main.cs
@@ -14,7 +14,7 @@ namespace Scan_for_Anything
     {
         private const string GUID = "me.greaterdane.subnautica.mod.scanforanything";
         private const string pluginName = "Scan for Anything";
-        private const string version = "1.1.0";
+        private const string version = "1.0.2";
 
         internal static new ConfigFields Config { get; } = new ConfigFields();
         private static readonly Harmony harmony = new Harmony(GUID);
